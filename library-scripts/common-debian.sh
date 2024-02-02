023d8a6c1cb673bcc266d81e3935603468085aa9
Derek Lewis <DerekNonGeneric@inf.is>
2024-02-02 16:26:40 -0500

    Update library-scripts/common-debian.sh
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

--- library-scripts/common-debian.sh
+++ library-scripts/common-debian.sh
@@ -5,7 +5,7 @@
 #-------------------------------------------------------------------------------------------------------------
 #
 # ** This script is community supported **
-# Docs: https://github.com/OpenINF/docker-fish/blob/HEAD/library-scripts/docs/common-debian.md
+# Docs: https://github.com/OpenINF/docker-fisher/blob/HEAD/library-scripts/docs/common-debian.md
 # Maintainer: The OpenINF Community
 #
 # Syntax: ./common-debian.sh [username] [user UID] [user GID] [upgrade packages flag] [Add non-free packages]
