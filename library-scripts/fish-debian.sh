209d5ccbe1dbfbc26e6b4f50caae05a45a48b3bc
Derek Lewis <dereknongeneric@open.inf.is>
2024-02-09 11:08:06 -0500

    would be great to rewrite this in pure fish, actually

--- library-scripts/fish-debian.sh
+++ library-scripts/fish-debian.sh
@@ -39,9 +39,9 @@ elif [ "$USERNAME" = "none" ] || ! id -u "$USERNAME" >/dev/null 2>&1; then
   USERNAME=root
 fi
 
-# Function to run apt-get if needed
+# Function to run apt-get if needed   
 apt_get_update_if_needed() {
-  if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+  if [ ! -d "/var/lib/apt/lists" ] || [ $(( $(wc -c < "/var/lib/apt/lists/") )) = "0" ]; then
     echo "Running apt-get update..."
     apt-get update
   else
