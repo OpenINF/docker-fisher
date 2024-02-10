9725b71a93c88b2e20caacebb09b66f1ea6f3695
Derek Lewis <DerekNonGeneric@inf.is>
2024-02-10 14:15:57 -0500

    Update build/tasks/util.mjs
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

--- build/tasks/util.mjs
+++ build/tasks/util.mjs
@@ -1,6 +1,6 @@
 import text from '@openinf/util-text';
 import url from 'node:url';
-
+import path from 'node:path';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
