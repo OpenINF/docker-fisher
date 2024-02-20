c4e7a1e021191ff1ba68741eea25a8e62b7a6098
Derek Lewis <dereknongeneric@open.inf.is>
2024-02-19 20:39:06 -0800

    the finishing touches

--- build/tasks/util.mjs
+++ build/tasks/util.mjs
@@ -1,8 +1,31 @@
 import text from '@openinf/util-text';
 import url from 'node:url';
-import path from 'node:path';
+import { chalk, echo, os, path } from 'zx';
+
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+/**
+ * Helper to make it possible to use absolute project paths.
+ * @param  {string} relativePath A path relative to the project root
+ * @return {string} An absolute ready-to-use path
+ */
+function absolute(relativePath) {
+  return path.resolve(path.dirname(path.dirname(__dirname)), relativePath);
+}
+
+/**
+ * Various paths that are critical to the project and used throughout.
+ */
+export const PATHS = {
+  projectRoot: absolute('.'),
+  manifest: absolute('./package.json'),
+};
+
+function taskURLToRelativePath(taskURL) {
+  const taskPath = url.fileURLToPath(taskURL);
+  return path.relative(PATHS.projectRoot, taskPath);
+}
 
 /**
  * Helper to make it possible to use absolute project paths.
