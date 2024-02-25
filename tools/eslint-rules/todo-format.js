77d545db851db1da98fde64d8ef374924d17700e
Restyled.io <commits@restyled.io>
2024-02-25 06:55:16 +0000

    Restyled by clang-format

--- tools/eslint-rules/todo-format.js
+++ tools/eslint-rules/todo-format.js
@@ -17,20 +17,15 @@
 
 module.exports = function(context) {
   return {
-    Program: function(node) {
+    Program : function(node) {
       if (node.comments) {
         node.comments.forEach(function(comment) {
-          if (
-            /TODO/.test(comment.value) &&
-            !/TODO\(@?\w+,\s*#\d{1,}\)/.test(comment.value)
-          ) {
+          if (/TODO/.test(comment.value) &&
+              !/TODO\(@?\w+,\s*#\d{1,}\)/.test(comment.value)) {
             context.report({
-              node: comment,
-              message:
-                'TODOs must be in TODO(@username, #1234) format. ' +
-                'Found: "' +
-                comment.value.trim() +
-                '"',
+              node : comment,
+              message : 'TODOs must be in TODO(@username, #1234) format. ' +
+                            'Found: "' + comment.value.trim() + '"',
             });
           }
         });
