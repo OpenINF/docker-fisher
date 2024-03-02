2096c1cb29a8ceab2b8055d143e143ff72f20031
Derek Lewis <dereknongeneric@open.inf.is>
2024-03-01 19:37:00 -0500

    add more draft code/pipe filler
    this is just to give y'all an idea real quick; refactor necessary

---
+++ tools/eslint-rules/index.mjs
@@ -0,0 +1,52 @@
+/**
+ * @file Index of internal ESLint rules.
+ * @author Derek Lewis <DerekNonGeneric@inf.is>
+ * @license 0BSD
+ * @module {EsModule} tools/eslint-rules/index
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+import { createRequire } from 'module';
+import {
+  dirname as pathDirname,
+  extname as pathExtname,
+  join as pathJoin,
+} from 'path';
+import { fileURLToPath as pathFromFileUrl } from 'url';
+import { readdirSync } from 'fs';
+import noAssignMutatedArray from './no-assign-mutated-array.mjs';
+
+const __filename = pathFromFileUrl(import.meta.url);
+const __dirname = pathDirname(__filename);
+const require = createRequire(import.meta.url);
+
+//------------------------------------------------------------------------------
+// Main
+//------------------------------------------------------------------------------
+
+const rules = { 'no-assign-mutated-array': noAssignMutatedArray };
+const ruleFiles = readdirSync(__dirname).filter(
+  (ruleFile) =>
+    ![
+      'index.js',
+      'index.mjs',
+      '.gitignore',
+      'node_modules',
+      'rules-utils.js',
+      'html-template.js',
+      'no-assign-mutated-array.mjs',
+      'no-log-array.js',
+      'no-mixed-interpolation.js',
+      'documented-errors.js',
+    ].includes(ruleFile)
+);
+
+ruleFiles.forEach(function (ruleFile) {
+  const rule = ruleFile.replace(pathExtname(ruleFile), '');
+  rules[rule] = require(pathJoin(__dirname, rule));
+});
+
+export default { rules };
